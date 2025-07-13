6fa5458bd45bf6f8637a6aa39f2d4aa6375eaddd
Abdelrahman Nashat <47497135+abdelrahmanashat@users.noreply.github.com>
2025-07-13 21:15:43 +0300

    Update london_baseline.py

--- a5/src/london_baseline.py
+++ a5/src/london_baseline.py
@@ -2,3 +2,19 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+from tqdm import tqdm
+import utils
+import argparse
+
+argp = argparse.ArgumentParser()
+argp.add_argument('--eval_corpus_path', default="birth_dev.tsv")
+args = argp.parse_args()
+
+correct = 0
+total = 0
+predictions = []
+for line in tqdm(open(args.eval_corpus_path, encoding='utf-8')):
+    predictions.append("London")
+total, correct = utils.evaluate_places(args.eval_corpus_path, predictions)
+print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
+
